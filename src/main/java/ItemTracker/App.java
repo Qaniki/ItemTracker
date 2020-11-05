f5a88b7a7fb17d0322851fad83ff78cf18e3644a
René Schou <r3neschou@gmail.com>
2020-11-05 22:57:11 +0100

    version 1.1 done

--- src/main/java/ItemTracker/App.java
+++ src/main/java/ItemTracker/App.java
@@ -1,24 +1,81 @@
 package ItemTracker;
 import java.util.Scanner;
 public class App {
-    private int setYear;
     private ItemTracker itemTracker;
+    private Scanner reader;
     // TODO: generate tests
     public App() {
-        this.setYear = 2020;
+        this.reader = new Scanner(System.in);
         this.itemTracker = addStartingItems();
     }
-    public void setYear(int year){
-        this.setYear = year;
-    }
-    public int getYear(){
-        return this.setYear;
-    }
-    public void setYear(ItemTracker itemTracker){
+    public void setItemTracker(ItemTracker itemTracker){
         this.itemTracker = itemTracker;
     }
     public ItemTracker getItemTracker(){
         return this.itemTracker;
+    }
+
+    public void start(){
+        System.out.println("\n\nWelcome to Item Tracker 420");
+        commandDisplay();
+        boolean endProgram = false;
+        while (!endProgram) {
+            String command = reader.nextLine().toLowerCase();
+            endProgram = processInput(command);
+        }
+        System.out.println("Program over");
+        reader.close();
+    }
+
+    private boolean processInput(String input){
+        boolean toQuit = false;
+
+        switch (input) {
+            case "commands":
+                commandDisplay();
+                break;
+            
+            case "list":
+                itemTracker.printAll();
+                break;
+
+            case "wealth":
+                itemTracker.printWealth();
+                break;
+
+            case "set year":
+                setYearApp(Integer.valueOf(reader.nextLine()));
+                break;
+            
+            case "add":
+                addItemApp();
+                break;
+            
+            case "remove":
+                removeItemApp();
+                break;
+
+            case "search":
+                searchItem();
+                break;
+
+            case "quit":
+                toQuit = true;
+                break;
+
+            default :
+                System.out.println("Unknown command");
+                break;
+        }
+        return toQuit;
+    }
+
+    private void commandDisplay(){
+        System.out.printf("Commands:\nSearch - Search for items\nStop - stops the program\n");
+        System.out.printf("List - List all items\nWealth - Sums of values of all items\n");
+        System.out.printf("Add - Add new item\nRemove - Remove an item\n");
+        System.out.printf("Set year - Set current year. default 2020\nCommands - Go back to command list\n");
+        System.out.println("\n\nEnter command:");
     }
 
     private ItemTracker addStartingItems(){
@@ -32,123 +89,83 @@ public class App {
         return itemTracker;
     }
 
-    private void commandDisplay(){
-        System.out.printf("Commands:\nSearch - Search for items\nStop - stops the program\n");
-        System.out.printf("List - List all items\nWealth - Sums of values of all items\n");
-        System.out.printf("Add - Add new item\nRemove - Remove an item\n");
-        System.out.printf("Set year - Set current year. default 2020\nCommands - Go back to command\n");
-        System.out.println("\n\nEnter command:");
+    private void setYearApp(int year){
+        System.out.println("Insert Year:");
+        itemTracker.setYear(year);
+        System.out.println("The year is set to " + itemTracker.getYear());
     }
 
-    private void start(){
-        System.out.println("\n\nWelcome to Item Tracker 420");
-        commandDisplay();
+    private void addItemApp(){
+        System.out.println("Name:");
+        String itemName = reader.nextLine();
+        System.out.println("Description:");
+        String itemDescription = reader.nextLine();
+        System.out.println("Purchase Price:");
+        double itemPrice = Double.valueOf(reader.nextLine());
+        System.out.println("Year of Purchase:");
+        int itemYear = Integer.valueOf(reader.nextLine());
+        System.out.println("Type/category:");
+        String itemType = reader.nextLine();
+        itemType = itemType.substring(0,1).toUpperCase() + itemType.substring(1).toLowerCase();
+        itemTracker.addItem(new Item(itemName, itemDescription, itemPrice, itemYear, itemType));
+        System.out.println("The item " + itemName + " is added.");
     }
+    private void removeItemApp(){
+        System.out.println("Name of item to remove:");
+        String nameToRemove = reader.nextLine();
+        String nameLowerCase = nameToRemove.toLowerCase();
+        boolean removed = itemTracker.removeItemByName(nameLowerCase);
+        System.out.println(removed ? nameToRemove + " has been removed." : "Can not find item " + nameToRemove);
+    }
+    private void searchItem(){
+        System.out.printf("Search commands:\nName - Search by name\nYear - Search by Year\n");
+        System.out.printf("Type - Search by Type/category\nPrice - Search by Price Threshold\n");
+        String searchCommand = reader.nextLine().toLowerCase();
+        switch (searchCommand) {
+            case "name":
+                System.out.println("Name to search:");
+                String searchName = reader.nextLine().toLowerCase();
+                itemTracker.getItems().stream()
+                .filter(t -> t.getName().toLowerCase().equals(searchName))
+                .map(Item::toString)
+                .forEach(System.out::println);
+                break;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        App app = new App();
-
-        String command = sc.nextLine().toLowerCase();
-
-        while(!command.equals("stop")){
+            case "year":
+                System.out.println("Year to search:");
+                int searchYear = Integer.valueOf(reader.nextLine());
+                itemTracker.getItems().stream()
+                .filter(t -> t.getYear() == searchYear)
+                .map(Item::toString)
+                .forEach(System.out::println);
+                break;
             
-            if(command.equals("list")){
-                it.printAll();
-            }
-            if(command.equals("command")||command.equals("commands")){
-                commandDisplay();
-            }
-            if(command.equals("wealth")){
-                int sum = 0;
-                for(Item item : it.getItems()){
-                    sum += item.getCurrentPrice(app.getYear());
-                }
-                System.out.println("The total value(value in "+app.getYear()+
-                ") of all items being tracked is "+sum);
-            }
-            if(command.equals("set year")||command.equals("set")){
-                System.out.println("Insert Year:");
-                app.setYear = Integer.valueOf(sc.nextLine());
-                System.out.println("The year is set to " + app.getYear());
-            }
-            if(command.equals("add")){
-                System.out.println("Name:");
-                String newname = sc.nextLine();
-                System.out.println("Description:");
-                String newdescription = sc.nextLine();
-                System.out.println("Purchase Price:");
-                double newprice = Double.valueOf(sc.nextLine());
-                System.out.println("Year of Purchase:");
-                int newyear = Integer.valueOf(sc.nextLine());
-                System.out.println("Type/category:");
-                String newtype = sc.nextLine();
-                newtype = newtype.substring(0,1).toUpperCase() + newtype.substring(1).toLowerCase();
-                it.addItem(new Item(newname, newdescription, newprice, newyear, newtype));
-                System.out.println("The item " + newname + " is added.");
-            }
-            if(command.equals("remove")){
-                System.out.println("Name of item to remove:");
-                String t = sc.nextLine();
-                String removename = t.toLowerCase();
-                boolean found = false;
-                for(Item item : it.getItems()){
-                    if(item.getName().toLowerCase().equals(removename)){
-                        it.removeItem(item);
-                        found = true;
-                        System.out.println(t + " has been removed.");
-                        break;
-                    }
-                }
-                if(!found){
-                    System.out.println("Can not find item " + t);
-                }
-            }
-            if(command.equals("search")){
-                System.out.printf("Search commands:\nName - Search by name\nYear - Search by Year\n");
-                System.out.printf("Type - Search by Type/category\nPrice - Search by Price Threshold\n");
-                command = sc.nextLine().toLowerCase();
-                if(command.equals("name")){
-                    System.out.println("Name to search:");
-                    String searchname = sc.nextLine().toLowerCase();
-                    it.getItems().stream()
-                    .filter(t -> t.getName().toLowerCase().equals(searchname))
-                    .map(Item::toString)
-                    .forEach(System.out::println);
-                }
-                if(command.equals("year")){
-                    System.out.println("Year to search:");
-                    int searchyear = Integer.valueOf(sc.nextLine());
-                    it.getItems().stream()
-                    .filter(t -> t.getYear() == searchyear)
-                    .map(Item::toString)
-                    .forEach(System.out::println);
-                }
-                if(command.equals("type")){
-                    System.out.println("Type to search:");
-                    String searchtype = sc.nextLine().toLowerCase();
-                    it.getItems().stream()
-                    .filter(t -> t.getType().toLowerCase().equals(searchtype))
-                    .map(Item::toString)
-                    .forEach(System.out::println);
-                }
-                if(command.equals("price")){
-                    System.out.println("Price to search:");
-                    Double searchprice = Double.valueOf(sc.nextLine());
-                    it.getItems().stream()
-                    .filter(t -> t.getPrice() < searchprice)
-                    .map(Item::toString)
-                    .forEach(System.out::println);
-                }
-            }
-            command = sc.nextLine();
+            case "type":
+                System.out.println("Type to search:");
+                String searchType = reader.nextLine().toLowerCase();
+                itemTracker.getItems().stream()
+                .filter(t -> t.getType().toLowerCase().equals(searchType))
+                .map(Item::toString)
+                .forEach(System.out::println);
+                break;
+            
+            case "price":
+                System.out.println("Price to search:");
+                Double searchPrice = Double.valueOf(reader.nextLine());
+                itemTracker.getItems().stream()
+                .filter(t -> t.getPrice() < searchPrice)
+                .map(Item::toString)
+                .forEach(System.out::println);
+                break;
+        
+            default:
+                System.out.println("Unknown search format");
+                break;
         }
-        sc.close();
     }
-
     
-
-   
-
-    
+    public static void main(String[] args) {
+        App app = new App();
+        app.start();
+    }
 }
--- src/main/java/ItemTracker/ItemTracker.java
+++ src/main/java/ItemTracker/ItemTracker.java
@@ -3,15 +3,32 @@ import java.util.List;
 import java.util.ArrayList;
 public class ItemTracker {
     private List<Item> items;
+    private int year;
 
     public ItemTracker() {
         this.items = new ArrayList<>();
+        this.year = 2020;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
     }
     public void addItem(Item item){
         this.items.add(item);
     }
     public void removeItem(Item item){
         this.items.remove(item);
+    }
+    public boolean removeItemByName(String name){
+        for(Item item : items){
+            if(item.getName().toLowerCase().equals(name)){
+                items.remove(item);
+                return true;
+            }
+        }
+        return false;
     }
     public List<Item> getItems() {
         return items;
@@ -21,4 +38,14 @@ public class ItemTracker {
             System.out.println(item.toString());
         }
     }
+    public void printWealth(){
+        int sum = 0;
+        for(Item item : getItems()){
+            sum += item.getCurrentPrice(year);
+        }
+        System.out.println("The total value(value in "+year+
+        ") of all items being tracked is "+sum);
+    }
+
+  
 }
